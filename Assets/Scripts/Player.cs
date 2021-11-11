df94c93aa1160030f360beccb7f71b4ea8e1d934
sherryysj <45263452+sherryysj@users.noreply.github.com>
2021-11-11 23:57:31 +1100

    Artwork: change play sprite direction when player turns direction

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,14 @@ public class Player : MonoBehaviour
 {
     public float moveSpeed = 3;
 
+    private SpriteRenderer sr;
+    public Sprite[] tankSprite; // up, right, down, left
+
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
     void Start()
     {
 
@@ -14,13 +22,32 @@ public class Player : MonoBehaviour
     void Update()
     {
 
-        // play horizantal move
+        // play horizontal move
         float h = Input.GetAxisRaw("Horizontal");
         transform.Translate(Vector3.right * h * moveSpeed * Time.deltaTime, Space.World);
+
+        // art work: change sprite to show player face which direction when play turn direction horizontally
+        if (h < 0)
+        {
+            sr.sprite = tankSprite[3];
+        } else if (h > 0) 
+        {
+            sr.sprite = tankSprite[1];
+        }
 
         // play vertical move
         float v = Input.GetAxisRaw("Vertical");
         transform.Translate(Vector3.up * v * moveSpeed * Time.deltaTime, Space.World);
+
+        // art work: change sprite to show player face which direction when play turn direction vertically
+        if (v < 0)
+        {
+            sr.sprite = tankSprite[2];
+        }
+        else if (v > 0)
+        {
+            sr.sprite = tankSprite[0];
+        }
 
     }
 }
