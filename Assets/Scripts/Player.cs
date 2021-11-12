cbfe70f1a91c510150fb298dbc8a0e3dd6c10a48
sherryysj <45263452+sherryysj@users.noreply.github.com>
2021-11-12 17:40:55 +1100

    Add collider and rigidbody to certain object and fix player move bugs to make it more smoothly

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    // player variables
     public float moveSpeed = 3;
 
+    // player tank artwork reference
     private SpriteRenderer sr;
     public Sprite[] tankSprite; // up, right, down, left
 
@@ -19,25 +22,39 @@ public class Player : MonoBehaviour
 
     }
 
-    void Update()
+    // Use fixed update to avoid player shaking problem
+    private void FixedUpdate()
     {
+        Move();
 
+    }
+
+    // player move
+    private void Move()
+    {
         // play horizontal move
         float h = Input.GetAxisRaw("Horizontal");
-        transform.Translate(Vector3.right * h * moveSpeed * Time.deltaTime, Space.World);
+        transform.Translate(Vector3.right * h * moveSpeed * Time.fixedDeltaTime, Space.World);
 
         // art work: change sprite to show player face which direction when play turn direction horizontally
         if (h < 0)
         {
             sr.sprite = tankSprite[3];
-        } else if (h > 0) 
+        }
+        else if (h > 0)
         {
             sr.sprite = tankSprite[1];
         }
 
+        // restrict player use both horizontal and vertical key at the same time to control the player
+        if (h != 0)
+        {
+            return;
+        }
+
         // play vertical move
         float v = Input.GetAxisRaw("Vertical");
-        transform.Translate(Vector3.up * v * moveSpeed * Time.deltaTime, Space.World);
+        transform.Translate(Vector3.up * v * moveSpeed * Time.fixedDeltaTime, Space.World);
 
         // art work: change sprite to show player face which direction when play turn direction vertically
         if (v < 0)
@@ -48,6 +65,5 @@ public class Player : MonoBehaviour
         {
             sr.sprite = tankSprite[0];
         }
-
     }
 }
