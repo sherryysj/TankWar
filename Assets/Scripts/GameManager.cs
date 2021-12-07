d4f52dd3b0814595c20a7f748b8a76a79bbe1a1b
sherryysj <45263452+sherryysj@users.noreply.github.com>
2021-12-07 20:56:43 +1100

    Home Scene added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -91,6 +92,12 @@ public class GameManager : MonoBehaviour
     public void GameOver()
     {
         gameOverUI.SetActive(true);
+        Invoke("ReturnHome", 3f);
+    }
+
+    public void ReturnHome()
+    {
+        SceneManager.LoadScene("Home");
     }
 
 }
